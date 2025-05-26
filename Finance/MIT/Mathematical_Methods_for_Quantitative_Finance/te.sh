7c56c682c29c5756dc38e29c496f76ae13a61166
Chaklader Asfak Arefe <omi.chaklader@gmail.com>
2025-05-26 19:07:43 +0600

    The commit is successful

---
+++ Finance/MIT/Mathematical_Methods_for_Quantitative_Finance/te.sh
@@ -0,0 +1,47 @@
+# mod 1
+wget https://courses.edx.org/assets/courseware/v1/9b8e5e8ac0093216e3c1ce47b256aaa1/asset-v1:MITx+15.455x+1T2025+type@asset+block/handouts-week01.pdf
+wget https://courses.edx.org/assets/courseware/v1/9a9a12527bb32a1c18e9c8addfdd839b/asset-v1:MITx+15.455x+1T2025+type@asset+block/recitation-week01v1.pdf
+
+wget https://courses.edx.org/assets/courseware/v1/d7124b8dfe5d5aea0834c71aa7d7d76d/asset-v1:MITx+15.455x+1T2025+type@asset+block/recitation-week01.R
+
+# mod 2
+wget https://courses.edx.org/asset-v1:MITx+15.455x+1T2025+type@asset+block@handouts-week02v2.pdf
+wget https://courses.edx.org/asset-v1:MITx+15.455x+1T2025+type@asset+block@recitation-week02v2.pdf
+
+wget https://courses.edx.org/asset-v1:MITx+15.455x+1T2025+type@asset+block@notebook-week02.Rmd
+wget https://courses.edx.org/asset-v1:MITx+15.455x+1T2025+type@asset+block@TR.csv
+wget https://courses.edx.org/asset-v1:MITx+15.455x+1T2025+type@asset+block@notebook-week02.Rmd
+
+# mod 3
+wget https://courses.edx.org/asset-v1:MITx+15.455x+1T2025+type@asset+block@handouts-week03v2.pdf
+
+wget https://courses.edx.org/asset-v1:MITx+15.455x+1T2025+type@asset+block@notebook-week03-TSmodels.Rmd
+wget https://courses.edx.org/asset-v1:MITx+15.455x+1T2025+type@asset+block@notebook-week03-TSmodels.nb.html
+
+
+# mod 4
+wget https://courses.edx.org/assets/courseware/v1/318db9fff5778958f0bdc290fb2d87ac/asset-v1:MITx+15.455x+1T2025+type@asset+block/handouts-week04v2.pdf
+wget https://courses.edx.org/assets/courseware/v1/bbd3cfae116c1784acbed530bf68e4f4/asset-v1:MITx+15.455x+1T2025+type@asset+block/recitation-week04v2.pdf
+
+
+
+# mod 5
+wget https://courses.edx.org/assets/courseware/v1/0a4f0f105c21dedacfdb7592177565fb/asset-v1:MITx+15.455x+1T2025+type@asset+block/handouts-week05v2.pdf
+wget https://courses.edx.org/assets/courseware/v1/9e1e9b6ddf884a73076266740418c638/asset-v1:MITx+15.455x+1T2025+type@asset+block/recitation-week05.pdf
+
+# mod 6
+wget https://courses.edx.org/assets/courseware/v1/bbbb9bfc2c40457156e6edecad24b687/asset-v1:MITx+15.455x+1T2025+type@asset+block/handouts-week06v2.pdf
+wget https://courses.edx.org/assets/courseware/v1/06f43cceff92fc53f2dee7e267ce2385/asset-v1:MITx+15.455x+1T2025+type@asset+block/recitation-week06.pdf
+
+# mod 7
+wget https://courses.edx.org/assets/courseware/v1/65cfc06bba1aecd97de3d0d01c149779/asset-v1:MITx+15.455x+1T2025+type@asset+block/handouts-week07.pdf
+wget https://courses.edx.org/assets/courseware/v1/0a26c5b816dcf9d6cb1ec2fa48db62f5/asset-v1:MITx+15.455x+1T2025+type@asset+block/recitation-week07v0.pdf
+
+# mod 8
+wget https://courses.edx.org/assets/courseware/v1/73646591d0b4f305ca8713852a151ebb/asset-v1:MITx+15.455x+1T2025+type@asset+block/handouts-week08.pdf
+
+wget https://courses.edx.org/assets/courseware/v1/a6ccce58ee8c18628840df750db43091/asset-v1:MITx+15.455x+1T2025+type@asset+block/notebook-week8-MVO.Rmd
+wget https://courses.edx.org/assets/courseware/v1/dcf652ab37615592bbd5cb9f9358a882/asset-v1:MITx+15.455x+1T2025+type@asset+block/michaud.csv
+
+# mod 9
+wget https://courses.edx.org/assets/courseware/v1/7c4c67c50d4c79c8319b044211283836/asset-v1:MITx+15.455x+1T2025+type@asset+block/handouts-week09.pdf
